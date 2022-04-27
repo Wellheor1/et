c325c55c30cd6810e5d7c79620d53957746c5230
Well <Well.heor@yandex.ru>
2022-04-27 11:02:38 +0800

    Модели БД

--- timesheetapp/admin.py
+++ timesheetapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from .models import Departments, Persons, TypesPosts, Posts, Employees, TimeWork
 # Register your models here.
+admin.site.register(Departments)
+admin.site.register(Persons)
+admin.site.register(TypesPosts)
+admin.site.register(Posts)
+admin.site.register(Employees)
+admin.site.register(TimeWork)
--- timesheetapp/models.py
+++ timesheetapp/models.py
@@ -1,3 +1,36 @@
 from django.db import models
 
-# Create your models here.
+
+class Departments(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Persons(models.Model):
+    last_name = models.CharField(max_length=255)
+    first_name = models.CharField(max_length=255)
+    patronymic = models.CharField(max_length=255)
+    snils = models.CharField(max_length=255)
+
+
+class TypesPosts(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Posts(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class Employees(models.Model):
+    person = models.ForeignKey(Persons, on_delete=models.CASCADE)
+    type_post = models.ForeignKey(TypesPosts, on_delete=models.CASCADE)
+    post = models.ForeignKey(Posts, on_delete=models.CASCADE)
+    department = models.ForeignKey(Departments, on_delete=models.CASCADE)
+    account_number = models.DecimalField(max_digits=10, decimal_places=2)
+
+
+class TimeWork(models.Model):
+    employee = models.ForeignKey(Employees, on_delete=models.CASCADE)
+    date = models.DateField(auto_now_add=True)
+    night_hours = models.DecimalField(max_digits=10, decimal_places=2)
+    all_hours = models.DecimalField(max_digits=10, decimal_places=2)
+
