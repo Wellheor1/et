d984d549772380a6be35aa5f7827b93c0564cc25
Well <Well.heor@yandex.ru>
2022-05-12 15:45:15 +0800

    tabel.xlsxv1

--- timesheetapp/tabel_xlsx.py
+++ timesheetapp/tabel_xlsx.py
@@ -1,11 +1,10 @@
-import datetime
 import calendar
-import copy
+import datetime
 
 import pytils as pytils
-
 from openpyxl import Workbook
 from openpyxl.styles import Alignment, NamedStyle, Font, Border, Side
+from openpyxl.worksheet.page import PageMargins
 
 department_table_number = 27
 month_name = pytils.dt.ru_strftime(u"%B", inflected=True, date=datetime.datetime.now())
@@ -15,7 +14,7 @@ first_day_month = 1
 last_day_month = calendar.monthrange(current_year, current_month)[1]
 tabel_type = 'первичный'
 hospital_name = 'ОГАУЗ ГИМДКБ'
-department_name = 'травмпункт'
+department_name = 'Кабинет неотложной травматологии и ортопедии (травмпункт)'
 date_now = datetime.datetime.now().strftime('%d.%m.%Y')
 main_doctor = 'Новожилов В.А.'
 head_department = 'Преториус Т.Л.'
@@ -28,14 +27,20 @@ active_list = book.active
 active_list.page_setup.orientation = active_list.ORIENTATION_LANDSCAPE
 active_list.page_setup.paperSize = active_list.PAPERSIZE_A4
 active_list.page_setup.fitToPage = 1
-active_list.title = f'{department_name}'
+active_list.page_margins = PageMargins(top=0.2, bottom=0.2, left=0.2, right=0.2)
+active_list.title = 'Табель 52Н'
 
 styleCenterTitle = NamedStyle(name='styleCenterTitle')
 styleCenterTitle.alignment = Alignment(horizontal='center', vertical='center')
 
+styleCenterSup = NamedStyle(name='styleCenterSup')
+styleCenterSup.font = Font(size=6)
+styleCenterSup.alignment = Alignment(horizontal='center', vertical='top')
+
 styleCenterBorder = NamedStyle(name='styleCenterTitleBorder')
 bd = Side(style='thin', color='000000')
 styleCenterBorder.border = Border(left=bd, right=bd, top=bd, bottom=bd)
+styleCenterBorder.font = Font(size=8)
 styleCenterBorder.alignment = Alignment(horizontal='center', vertical='center')
 
 styleCenterBorderBottom = NamedStyle(name='styleCenterBorderBottom')
@@ -48,85 +53,92 @@ styleCenterTitleBold.alignment = Alignment(horizontal='center', vertical='center
 styleCenterTitleBold.font = Font(bold=True, color='000000')
 
 styleRightTitle = NamedStyle(name='styleRightTitle')
+styleRightTitle.font = Font(size=8)
 styleRightTitle.alignment = Alignment(horizontal='right', vertical='center')
 
 styleLeftTitle = NamedStyle(name='styleLeftTitle')
+styleLeftTitle.font = Font(size=8)
 styleLeftTitle.alignment = Alignment(horizontal='left', vertical='center')
 
 styleRightTitleBold = NamedStyle(name='styleRightTitleBold')
 styleRightTitleBold.alignment = Alignment(horizontal='right', vertical='center')
-styleRightTitleBold.font = Font(bold=True, color='000000')
+styleRightTitleBold.font = Font(bold=True, color='000000', size=8)
 
 active_list.merge_cells(start_row=1, start_column=35, end_row=1, end_column=38)
+active_list['AI1'].value = 'Утв приказом Минфина России'
+active_list['AI1'].style = styleRightTitleBold
+
 active_list.merge_cells(start_row=2, start_column=35, end_row=2, end_column=38)
-d = active_list['AI1']
-d.value = 'Утв приказом Минфина России'
-d.style = styleRightTitleBold
-d = active_list['AI2']
-d.value = 'от 30 марта 2015г № 52н'
-d.style = styleRightTitleBold
+active_list['AI2'].value = 'от 30 марта 2015г № 52н'
+active_list['AI2'].style = styleRightTitleBold
 
-active_list.merge_cells(start_row=3, start_column=14, end_row=3, end_column=16)
-d = active_list['N3']
-d.value = f'Табель № {department_table_number}'
-d.style = styleCenterTitleBold
+active_list.merge_cells(start_row=3, start_column=4, end_row=3, end_column=30)
+active_list['D3'].value = f'Табель № {department_table_number}'
+active_list['D3'].style = styleCenterTitleBold
 
-active_list.merge_cells(start_row=4, start_column=13, end_row=4, end_column=17)
-d = active_list['M4']
-d.value = 'учета использования рабочего времени'
-d.style = styleCenterTitle
+active_list.merge_cells(start_row=4, start_column=4, end_row=4, end_column=30)
+active_list['D4'].value = 'учета использования рабочего времени'
+active_list['D4'].style = styleCenterTitle
 active_list.merge_cells(start_row=4, start_column=37, end_row=4, end_column=38)
-d = active_list['AK4']
-d.value = 'коды'
-d.style = styleCenterBorder
-d = active_list['AL4']
-d.style = styleCenterBorder
+active_list['AK4'].value = 'коды'
 
-active_list.merge_cells(start_row=5, start_column=35, end_row=5, end_column=36)
-d = active_list['AI5']
-d.value = 'форма ОКУД'
-d.style = styleRightTitle
+active_list['AJ5'].value = 'форма ОКУД'
+active_list['AJ5'].style = styleRightTitle
 active_list.merge_cells(start_row=5, start_column=37, end_row=5, end_column=38)
-d = active_list['AK5']
-d.value = f'{form_okyd}'
-d.style = styleCenterBorder
-d = active_list['AL5']
-d.style = styleCenterBorder
+active_list['AK5'].value = f'{form_okyd}'
 
-active_list.merge_cells(start_row=6, start_column=14, end_row=6, end_column=16)
-d = active_list['N6']
-d.value = f'За период с {first_day_month} по {last_day_month} {month_name}'
-d.style = styleCenterTitle
-d = active_list['AJ6']
-d.value = 'Дата'
-d.style = styleRightTitle
+active_list.merge_cells(start_row=6, start_column=4, end_row=6, end_column=30)
+active_list['D6'].value = f'За период с {first_day_month} по {last_day_month} {month_name}'
+active_list['D6'].style = styleCenterTitle
+active_list['AJ6'].value = 'Дата'
+active_list['AJ6'].style = styleRightTitle
 active_list.merge_cells(start_row=6, start_column=37, end_row=6, end_column=38)
-d = active_list['AK6']
-d.value = f'{date_now}'
-d.style = styleCenterBorder
-d = active_list['AL6']
-d.style = styleCenterBorder
+active_list['AK6'].value = f'{date_now}'
 
-d = active_list['A7']
-d.value = 'Учреждение'
-d.style = styleLeftTitle
-active_list.merge_cells(start_row=7, start_column=14, end_row=7, end_column=16)
-d = active_list['N7']
-d.value = f'{hospital_name}'
-d.style = styleCenterBorderBottom
-active_list.merge_cells(start_row=7, start_column=35, end_row=7, end_column=36)
-d = active_list['AI7']
-d.value = 'по ОКПО'
-d.style = styleRightTitle
+active_list['A7'].value = 'Учреждение'
+active_list['A7'].style = styleLeftTitle
+active_list.merge_cells(start_row=7, start_column=4, end_row=7, end_column=30)
+active_list['D7'].value = f'{hospital_name}'
+# active_list.merge_cells(start_row=7, start_column=35, end_row=7, end_column=36)
+active_list['AJ7'].value = 'по ОКПО'
+active_list['AJ7'].style = styleRightTitle
 active_list.merge_cells(start_row=7, start_column=37, end_row=7, end_column=38)
-d = active_list['AK7']
-d.style = styleCenterBorder
-d = active_list['AL7']
-d.style = styleCenterBorder
-thin = Border(bottom=)
-for row in active_list.iter_rows(min_row=7, min_col=3, max_row=7, max_col=30):
-    for cell in row:
-        cell.border = Border(bottom='thin')
+
+active_list.merge_cells(start_row=8, start_column=1, end_row=8, end_column=3)
+active_list['A8'].value = 'Структурное подразделение'
+active_list['A8'].style = styleLeftTitle
+active_list.merge_cells(start_row=8, start_column=4, end_row=8, end_column=30)
+active_list['D8'].value = f'{department_name}'
+active_list.merge_cells(start_row=8, start_column=37, end_row=8, end_column=38)
+
+active_list['A9'].value = 'Вид табеля'
+active_list['A9'].style = styleLeftTitle
+active_list.merge_cells(start_row=9, start_column=4, end_row=9, end_column=30)
+active_list['D9'].value = f'{tabel_type}'
+active_list.merge_cells(start_row=9, start_column=35, end_row=9, end_column=36)
+active_list['AI9'].value = 'Номер корректировки'
+active_list['AI9'].style = styleRightTitle
+active_list.merge_cells(start_row=9, start_column=37, end_row=9, end_column=38)
+
+active_list.merge_cells(start_row=10, start_column=4, end_row=10, end_column=30)
+active_list['D10'].value = '(первичный - 0; корректирующий 1,2 и т.д.)'
+active_list.merge_cells(start_row=10, start_column=34, end_row=10, end_column=36)
+active_list['AH10'].value = 'Дата формирования документа'
+active_list['AH10'].style = styleRightTitle
+active_list.merge_cells(start_row=10, start_column=37, end_row=10, end_column=38)
+active_list['AK10'].value = f'{date_now}'
+
+for column in active_list['AK4':'AL10']:
+    for cell in column:
+        cell.style = styleCenterBorder
+
+for column in active_list['D7':'AD9']:
+    for cell in column:
+        cell.style = styleCenterBorderBottom
+
+for column in active_list['D10':'AD10']:
+    for cell in column:
+        cell.style = styleCenterSup
 
 
 book.save('tabel.xlsx')
